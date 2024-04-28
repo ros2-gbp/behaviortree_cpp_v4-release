6e21a6c3988eadb8c8cd7f72284d11e465dfbcd6
Davide Faconti <davide.faconti@gmail.com>
2024-04-28 15:51:10 +0200

    Rebase from 'release/humble/behaviortree_cpp'

--- include/behaviortree_cpp/action_node.h
+++ include/behaviortree_cpp/action_node.h
@@ -116,8 +116,8 @@ protected:
 class ThreadedAction : public ActionNodeBase
 {
 public:
-  ThreadedAction(const std::string& name, const NodeConfig& config) :
-    ActionNodeBase(name, config)
+  ThreadedAction(const std::string& name, const NodeConfig& config)
+    : ActionNodeBase(name, config)
   {}
 
   bool isHaltRequested() const
@@ -132,7 +132,7 @@ public:
 
 private:
   std::exception_ptr exptr_;
-  std::atomic_bool halt_requested_;
+  std::atomic_bool halt_requested_ = false;
   std::future<void> thread_handle_;
   std::mutex mutex_;
 };
@@ -159,8 +159,8 @@ using AsyncActionNode = ThreadedAction;
 class StatefulActionNode : public ActionNodeBase
 {
 public:
-  StatefulActionNode(const std::string& name, const NodeConfig& config) :
-    ActionNodeBase(name, config)
+  StatefulActionNode(const std::string& name, const NodeConfig& config)
+    : ActionNodeBase(name, config)
   {}
 
   /// Method called once, when transitioning from the state IDLE.
@@ -183,7 +183,7 @@ protected:
   void halt() override final;
 
 private:
-  std::atomic_bool halt_requested_;
+  std::atomic_bool halt_requested_ = false;
 };
 
 /**
@@ -222,13 +222,12 @@ public:
   void halt() override;
 
 protected:
-  struct Pimpl;   // The Pimpl idiom
+  struct Pimpl;  // The Pimpl idiom
   std::unique_ptr<Pimpl> _p;
 
   void destroyCoroutine();
 };
 
-
-}   // namespace BT
+}  // namespace BT
 
 #endif
--- src/loggers/bt_sqlite_logger.cpp
+++ src/loggers/bt_sqlite_logger.cpp
@@ -2,15 +2,15 @@
 #include "behaviortree_cpp/xml_parsing.h"
 #include "cpp-sqlite/sqlite.hpp"
 
-namespace BT {
+namespace BT
+{
 
-SqliteLogger::SqliteLogger(const Tree &tree,
-                           std::filesystem::path const& filepath,
-                           bool append):
-  StatusChangeLogger(tree.rootNode())
+SqliteLogger::SqliteLogger(const Tree& tree, std::filesystem::path const& filepath,
+                           bool append)
+  : StatusChangeLogger(tree.rootNode())
 {
   const auto extension = filepath.filename().extension();
-  if( extension!= ".db3" && extension != ".btdb")
+  if(extension != ".db3" && extension != ".btdb")
   {
     throw RuntimeError("SqliteLogger: the file extension must be [.db3] or [.btdb]");
   }
@@ -19,24 +19,29 @@ SqliteLogger::SqliteLogger(const Tree &tree,
 
   db_ = std::make_unique<sqlite::Connection>(filepath.string());
 
-  sqlite::Statement(*db_,
-                    "CREATE TABLE IF NOT EXISTS Transitions ("
-                    "timestamp  INTEGER PRIMARY KEY NOT NULL, "
-                    "session_id INTEGER NOT NULL, "
-                    "uid        INTEGER NOT NULL, "
-                    "duration   INTEGER, "
-                    "state      INTEGER NOT NULL);");
+  sqlite::Statement(*db_, "CREATE TABLE IF NOT EXISTS Transitions ("
+                          "timestamp  INTEGER PRIMARY KEY NOT NULL, "
+                          "session_id INTEGER NOT NULL, "
+                          "node_uid   INTEGER NOT NULL, "
+                          "duration   INTEGER, "
+                          "state      INTEGER NOT NULL,"
+                          "extra_data VARCHAR );");
 
-  sqlite::Statement(*db_,
-                    "CREATE TABLE IF NOT EXISTS Definitions ("
-                    "session_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    "date       TEXT NOT NULL,"
-                    "xml_tree   TEXT NOT NULL);");
+  sqlite::Statement(*db_, "CREATE TABLE IF NOT EXISTS Nodes ("
+                          "session_id INTEGER NOT NULL, "
+                          "fullpath   VARCHAR, "
+                          "node_uid   INTEGER NOT NULL );");
 
-  if( !append )
+  sqlite::Statement(*db_, "CREATE TABLE IF NOT EXISTS Definitions ("
+                          "session_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                          "date       TEXT NOT NULL,"
+                          "xml_tree   TEXT NOT NULL);");
+
+  if(!append)
   {
     sqlite::Statement(*db_, "DELETE from Transitions;");
     sqlite::Statement(*db_, "DELETE from Definitions;");
+    sqlite::Statement(*db_, "DELETE from Nodes;");
   }
 
   auto tree_xml = WriteTreeToXML(tree, true, true);
@@ -45,12 +50,23 @@ SqliteLogger::SqliteLogger(const Tree &tree,
                     "VALUES (datetime('now','localtime'),?);",
                     tree_xml);
 
-  auto res = sqlite::Query(*db_, "SELECT MAX(session_id) FROM Definitions LIMIT 1;");
+  auto res = sqlite::Query(*db_, "SELECT MAX(session_id) "
+                                 "FROM Definitions LIMIT 1;");
 
   while(res.Next())
   {
     session_id_ = res.Get(0);
   }
+
+  for(const auto& subtree : tree.subtrees)
+  {
+    for(const auto& node : subtree->nodes)
+    {
+      sqlite::Statement(*db_, "INSERT INTO Nodes VALUES (?, ?, ?)", session_id_,
+                        node->fullPath(), node->UID());
+    }
+  }
+
   writer_thread_ = std::thread(&SqliteLogger::writerLoop, this);
 }
 
@@ -63,28 +79,30 @@ SqliteLogger::~SqliteLogger()
   sqlite::Statement(*db_, "PRAGMA optimize;");
 }
 
-void SqliteLogger::callback(Duration timestamp,
-                            const TreeNode &node,
-                            NodeStatus prev_status,
-                            NodeStatus status)
+void SqliteLogger::setAdditionalCallback(ExtraCallback func)
 {
+  extra_func_ = func;
+}
 
+void SqliteLogger::callback(Duration timestamp, const TreeNode& node,
+                            NodeStatus prev_status, NodeStatus status)
+{
   using namespace std::chrono;
   int64_t tm_usec = int64_t(duration_cast<microseconds>(timestamp).count());
-  monotonic_timestamp_ = std::max( monotonic_timestamp_ + 1, tm_usec);
+  monotonic_timestamp_ = std::max(monotonic_timestamp_ + 1, tm_usec);
 
   long elapsed_time = 0;
 
-  if( prev_status == NodeStatus::IDLE && status == NodeStatus::RUNNING )
+  if(prev_status == NodeStatus::IDLE && status == NodeStatus::RUNNING)
   {
     starting_time_[&node] = monotonic_timestamp_;
   }
 
-  if( prev_status == NodeStatus::RUNNING && status != NodeStatus::RUNNING )
+  if(prev_status == NodeStatus::RUNNING && status != NodeStatus::RUNNING)
   {
     elapsed_time = monotonic_timestamp_;
     auto it = starting_time_.find(&node);
-    if( it != starting_time_.end() )
+    if(it != starting_time_.end())
     {
       elapsed_time -= it->second;
     }
@@ -96,11 +114,26 @@ void SqliteLogger::callback(Duration timestamp,
   trans.node_uid = node.UID();
   trans.status = status;
 
+  if(extra_func_)
+  {
+    trans.extra_data = extra_func_(timestamp, node, prev_status, status);
+  }
+
   {
     std::scoped_lock lk(queue_mutex_);
     transitions_queue_.push_back(trans);
   }
   queue_cv_.notify_one();
+
+  if(extra_func_)
+  {
+    extra_func_(timestamp, node, prev_status, status);
+  }
+}
+
+void SqliteLogger::execSqlStatement(std::string statement)
+{
+  sqlite::Statement(*db_, statement);
 }
 
 void SqliteLogger::writerLoop()
@@ -112,9 +145,7 @@ void SqliteLogger::writerLoop()
     transitions.clear();
     {
       std::unique_lock lk(queue_mutex_);
-      queue_cv_.wait(lk, [this]() {
-        return !transitions_queue_.empty() || !loop_;
-      });
+      queue_cv_.wait(lk, [this]() { return !transitions_queue_.empty() || !loop_; });
       std::swap(transitions, transitions_queue_);
     }
 
@@ -123,14 +154,9 @@ void SqliteLogger::writerLoop()
       auto const trans = transitions.front();
       transitions.pop_front();
 
-      sqlite::Statement(
-          *db_,
-          "INSERT INTO Transitions VALUES (?, ?, ?, ?, ?)",
-          trans.timestamp,
-          session_id_,
-          trans.node_uid,
-          trans.duration,
-          static_cast<int>(trans.status));
+      sqlite::Statement(*db_, "INSERT INTO Transitions VALUES (?, ?, ?, ?, ?, ?)",
+                        trans.timestamp, session_id_, trans.node_uid, trans.duration,
+                        static_cast<int>(trans.status), trans.extra_data);
     }
   }
 }
@@ -140,4 +166,4 @@ void BT::SqliteLogger::flush()
   sqlite3_db_cacheflush(db_->GetPtr());
 }
 
-}
+}  // namespace BT
