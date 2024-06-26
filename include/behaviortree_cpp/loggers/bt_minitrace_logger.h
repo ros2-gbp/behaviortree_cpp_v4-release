8a99d5f5a3f3888eff6194d81475dc50b3ab742d
Davide Faconti <davide.faconti@gmail.com>
2024-06-26 11:42:41 +0200

    Rebase from 'upstream'

--- include/behaviortree_cpp/loggers/bt_minitrace_logger.h
+++ include/behaviortree_cpp/loggers/bt_minitrace_logger.h
@@ -1,15 +1,11 @@
-#ifndef BT_MINITRACE_LOGGER_H
-#define BT_MINITRACE_LOGGER_H
+#pragma once
 
-#include <cstring>
 #include "behaviortree_cpp/loggers/abstract_logger.h"
 
 namespace BT
 {
 class MinitraceLogger : public StatusChangeLogger
 {
-  static std::atomic<bool> ref_count;
-
 public:
   MinitraceLogger(const BT::Tree& tree, const char* filename_json);
 
@@ -25,5 +21,3 @@ private:
 };
 
 }  // namespace BT
-
-#endif  // BT_MINITRACE_LOGGER_H
