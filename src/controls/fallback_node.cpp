3e91e985e6ad43b1f51fc38f0033e945a532802b
Davide Faconti <davide.faconti@gmail.com>
2025-04-24 09:38:08 +0200

    Rebase from 'rpm/jazzy/behaviortree_cpp'

--- src/controls/fallback_node.cpp
+++ src/controls/fallback_node.cpp
@@ -16,10 +16,7 @@
 namespace BT
 {
 FallbackNode::FallbackNode(const std::string& name, bool make_asynch)
-  : ControlNode::ControlNode(name, {})
-  , current_child_idx_(0)
-  , all_skipped_(true)
-  , asynch_(make_asynch)
+  : ControlNode::ControlNode(name, {}), current_child_idx_(0), asynch_(make_asynch)
 {
   if(asynch_)
     setRegistrationID("AsyncFallback");
@@ -33,7 +30,7 @@ NodeStatus FallbackNode::tick()
 
   if(status() == NodeStatus::IDLE)
   {
-    all_skipped_ = true;
+    skipped_count_ = 0;
   }
 
   setStatus(NodeStatus::RUNNING);
@@ -44,9 +41,6 @@ NodeStatus FallbackNode::tick()
 
     auto prev_status = current_child_node->status();
     const NodeStatus child_status = current_child_node->executeTick();
-
-    // switch to RUNNING state as soon as you find an active child
-    all_skipped_ &= (child_status == NodeStatus::SKIPPED);
 
     switch(child_status)
     {
@@ -73,6 +67,7 @@ NodeStatus FallbackNode::tick()
       case NodeStatus::SKIPPED: {
         // It was requested to skip this node
         current_child_idx_++;
+        skipped_count_++;
       }
       break;
       case NodeStatus::IDLE: {
@@ -89,7 +84,7 @@ NodeStatus FallbackNode::tick()
   }
 
   // Skip if ALL the nodes have been skipped
-  return all_skipped_ ? NodeStatus::SKIPPED : NodeStatus::FAILURE;
+  return (skipped_count_ == children_count) ? NodeStatus::SKIPPED : NodeStatus::FAILURE;
 }
 
 void FallbackNode::halt()
