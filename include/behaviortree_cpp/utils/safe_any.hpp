5281d71d4241672801e3bf7e966ff5363e0f509f
Davide Faconti <davide.faconti@gmail.com>
2025-12-30 11:46:37 +0100

    Rebase from 'release/rolling/behaviortree_cpp'

--- include/behaviortree_cpp/utils/safe_any.hpp
+++ include/behaviortree_cpp/utils/safe_any.hpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2022 Davide Faconti -  All Rights Reserved
+/* Copyright (C) 2022-2025 Davide Faconti -  All Rights Reserved
 *
 *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 *   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -70,7 +70,8 @@ public:
   Any(const Any& other) : _any(other._any), _original_type(other._original_type)
   {}
 
-  Any(Any&& other) : _any(std::move(other._any)), _original_type(other._original_type)
+  Any(Any&& other) noexcept
+    : _any(std::move(other._any)), _original_type(other._original_type)
   {}
 
   explicit Any(const double& value) : _any(value), _original_type(typeid(double))
@@ -116,6 +117,8 @@ public:
   }
 
   Any& operator=(const Any& other);
+
+  Any& operator=(Any&& other) noexcept;
 
   [[nodiscard]] bool isNumber() const;
 
@@ -305,7 +308,17 @@ inline bool isCastingSafe(const std::type_index& type, const T& val)
 
 inline Any& Any::operator=(const Any& other)
 {
-  this->_any = other._any;
+  if(this != &other)
+  {
+    this->_any = other._any;
+    this->_original_type = other._original_type;
+  }
+  return *this;
+}
+
+inline Any& Any::operator=(Any&& other) noexcept
+{
+  this->_any = std::move(other._any);
   this->_original_type = other._original_type;
   return *this;
 }
