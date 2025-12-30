150f6d1e201c06475d541c05500bb7d2a106ba53
Davide Faconti <davide.faconti@gmail.com>
2025-12-30 11:39:40 +0100

    Rebase from 'debian/jazzy/behaviortree_cpp'

--- src/controls/sequence_node.cpp
+++ src/controls/sequence_node.cpp
@@ -1,5 +1,5 @@
 /* Copyright (C) 2015-2018 Michele Colledanchise -  All Rights Reserved
- * Copyright (C) 2018-2020 Davide Faconti, Eurecat -  All Rights Reserved
+ * Copyright (C) 2018-2025 Davide Faconti, Eurecat -  All Rights Reserved
 *
 *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 *   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -15,18 +15,24 @@
 
 namespace BT
 {
-SequenceNode::SequenceNode(const std::string& name, bool make_async)
-  : ControlNode::ControlNode(name, {}), current_child_idx_(0), asynch_(make_async)
+SequenceNode::SequenceNode(const std::string& name, bool make_async,
+                           const NodeConfiguration& conf)
+  : ControlNode::ControlNode(name, conf), current_child_idx_(0), asynch_(make_async)
 {
   if(asynch_)
+  {
     setRegistrationID("AsyncSequence");
+  }
   else
+  {
     setRegistrationID("Sequence");
+  }
 }
 
 void SequenceNode::halt()
 {
   current_child_idx_ = 0;
+  skipped_count_ = 0;
   ControlNode::halt();
 }
 
@@ -34,7 +40,7 @@ NodeStatus SequenceNode::tick()
 {
   const size_t children_count = children_nodes_.size();
 
-  if(status() == NodeStatus::IDLE)
+  if(!isStatusActive(status()))
   {
     skipped_count_ = 0;
   }
@@ -62,7 +68,7 @@ NodeStatus SequenceNode::tick()
       case NodeStatus::SUCCESS: {
         current_child_idx_++;
         // Return the execution flow if the child is async,
-        // to make this interruptable.
+        // to make this interruptible.
         if(asynch_ && requiresWakeUp() && prev_status == NodeStatus::IDLE &&
            current_child_idx_ < children_count)
         {
@@ -86,13 +92,15 @@ NodeStatus SequenceNode::tick()
   }    // end while loop
 
   // The entire while loop completed. This means that all the children returned SUCCESS.
+  const bool all_children_skipped = (skipped_count_ == children_count);
   if(current_child_idx_ == children_count)
   {
     resetChildren();
     current_child_idx_ = 0;
+    skipped_count_ = 0;
   }
   // Skip if ALL the nodes have been skipped
-  return (skipped_count_ == children_count) ? NodeStatus::SKIPPED : NodeStatus::SUCCESS;
+  return (all_children_skipped) ? NodeStatus::SKIPPED : NodeStatus::SUCCESS;
 }
 
 }  // namespace BT
