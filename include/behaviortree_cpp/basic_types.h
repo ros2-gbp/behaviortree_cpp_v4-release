f6d506da599467375dffcd62927b2a7ab11b287a
Davide Faconti <davide.faconti@gmail.com>
2026-02-11 10:24:41 +0100

    Rebase from 'release/jazzy/behaviortree_cpp'

--- include/behaviortree_cpp/basic_types.h
+++ include/behaviortree_cpp/basic_types.h
@@ -1,18 +1,18 @@
 #pragma once
 
+#include "behaviortree_cpp/contrib/expected.hpp"
+#include "behaviortree_cpp/exceptions.h"
+#include "behaviortree_cpp/utils/safe_any.hpp"
+
 #include <chrono>
-#include <iostream>
 #include <functional>
+#include <iostream>
 #include <string_view>
 #include <typeinfo>
 #include <unordered_map>
 #include <utility>
 #include <variant>
 #include <vector>
-
-#include "behaviortree_cpp/utils/safe_any.hpp"
-#include "behaviortree_cpp/exceptions.h"
-#include "behaviortree_cpp/contrib/expected.hpp"
 
 namespace BT
 {
@@ -347,6 +347,11 @@ struct Timestamp
 [[nodiscard]] bool IsAllowedPortName(StringView str);
 
 [[nodiscard]] bool IsReservedAttribute(StringView str);
+
+/// Returns the first forbidden character found in the name, or '\0' if valid.
+/// Forbidden characters include: space, tab, newline, CR, < > & " ' / \ : * ? | .
+/// and control characters (ASCII 0-31, 127). UTF-8 multibyte sequences are allowed.
+[[nodiscard]] char findForbiddenChar(StringView name);
 
 class TypeInfo
 {
